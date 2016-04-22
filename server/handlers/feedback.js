6ca54c31e468ec2618b814f9fe5fbfffef497e42
Joe Bazooka <Joe Bazooka>
2016-04-22 20:37:54 +0300

    feedback handler refactored

--- server/handlers/feedback.js
+++ server/handlers/feedback.js
@@ -1,69 +1,69 @@
 var db = require('../db/config');
 
+function getAllFeedbacks(req, res) {
+  db.Feedback.find({}, function(err, feedbacks){
+
+    if (err) {
+      res.json({
+        success: false,
+        errors: err
+      })
+      return;
+    }
+
+    res.json({
+      success: true,
+      feedbacks: feedbacks,
+      types: db.Feedback.schema.path('feedbackType').enumValues,
+      statuses: db.Feedback.schema.path('status').enumValues,
+    })
+  })
+}
+
+function createFeedback(req, res) {
+  db.Feedback.create(req.body, function(err, feedback){
+    if (err) {
+      res.json({
+        success: false,
+        errors: err
+      })
+      return;
+    }
+
+    res.json({
+      success: true
+    })
+  })
+}
+
+function deleteFeedback(req, res) {
+  db.Feedback.findByIdAndRemove(req.params.id, function (err, feedback) {
+    if(err) {
+      res.json({
+        success: false,
+        errors: err
+      })
+      return;
+    }
+
+    res.json({
+      success: true
+    })
+  })
+}
+
+function editFeedback(req, res) {
+    var id = req.params.id;
+    db.Feedback.findByIdAndUpdate(id, req.body, {new: true}, function(err, updated) {
+    res.json({
+      success: true
+    })
+  })
+}
+
 module.exports = {
-
-  getAllFeedbacks: function(req, res) {
-
-    db.Feedback.find({}, function(err, feedbacks){
-
-      if (err) {
-        res.json({
-          success: false,
-          errors: err
-        })
-        return;
-      }
-
-      res.json({
-        success: true,
-        feedbacks: feedbacks,
-        types: db.Feedback.schema.path('feedbackType').enumValues,
-        statuses: db.Feedback.schema.path('status').enumValues,
-      })
-    })
-
-  },
-
-  createFeedback: function(req, res) {
-
-    db.Feedback.create(req.body, function(err, feedback){
-
-      if (err) {
-        res.json({
-          success: false,
-          errors: err
-        })
-        return;
-      }
-
-      res.json({
-        success: true
-      })
-    })
-
-  },
-
-  deleteFeedback: function (req, res) {
-
-    db.Feedback.findByIdAndRemove(req.params.id, function (err, feedback) {
-      if(err) {
-        res.json({
-          success: false,
-          errors: err
-        })
-        return;
-      }
-
-      res.json({
-        success: true
-      })
-    })
-
-  },
-
-  editFeedback: function (req, res) {
-
-    db.Feedback.findOneAndUpdate(req.params.id,)
-  }
-
+  getAllFeedbacks: getAllFeedbacks,
+  createFeedback: createFeedback,
+  deleteFeedback: deleteFeedback,
+  editFeedback: editFeedback
 }
